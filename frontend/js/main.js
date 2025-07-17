f63e9d630048447aef37a11c845f64af8b2ac5d6
Nicolás Foti <nicolasdfoti@gmail.com>
2025-07-17 12:36:44 -0300

    Added fetch to JS

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -23,7 +23,7 @@ form.addEventListener("submit", async (event) => {
     console.log(formData);
 
     try {
-        const response = await fetch("http://localhost:3000/loads", {
+        const response = await fetch("https://trackpool-api.onrender.com/loads", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
